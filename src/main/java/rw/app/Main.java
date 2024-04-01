05a702bd7cc5a47d8851901082bf10ad62f33ff3
farshad.islam <farshad.islam@ucalgary.ca>
2024-04-01 13:30:44 -0600

    Enter key

--- src/main/java/rw/app/Main.java
+++ src/main/java/rw/app/Main.java
@@ -3,6 +3,7 @@ package rw.app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,6 +22,7 @@ public class Main extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
 
         //Students edit here to set up the scene
+
         stage.setTitle("Robot Wars World Editor (v" + version + ")");
         stage.setScene(scene);
         stage.show();
