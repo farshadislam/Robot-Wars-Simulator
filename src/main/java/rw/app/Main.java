9c8bddb1a6c1d64f8602087e69033fdd35661609
farshad.islam <farshad.islam@ucalgary.ca>
2024-04-04 00:15:15 -0600

    removed redundant imports

--- src/main/java/rw/app/Main.java
+++ src/main/java/rw/app/Main.java
@@ -1,12 +1,9 @@
 package rw.app;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 
