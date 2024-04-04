8f6477d933f7eff79562d72418c87d611e3c2eb3
farshad.islam <farshad.islam@ucalgary.ca>
2024-04-03 22:26:49 -0600

    Logic for saving and loading

--- src/main/java/rw/app/MainController.java
+++ src/main/java/rw/app/MainController.java
@@ -18,6 +18,7 @@ import java.io.File;
 import javafx.stage.Stage;
 import rw.battle.*;
 import rw.enums.WeaponType;
+import rw.util.Reader;
 
 public class MainController{
 
@@ -43,14 +44,16 @@ public class MainController{
     private TextArea displayButtonInfo;
 
     @FXML
-    private Label errorStatus;
+    private Label errorStatus, fileStatus;
 
     @FXML
     private AnchorPane anchorGridPane = new AnchorPane();
 
-    private GridPane grid;
+    private GridPane grid = new GridPane();
 
     private Button[][] buttons;
+
+    private File mainBattleFile;
 
     /**
      * Set up the window state
@@ -62,26 +65,31 @@ public class MainController{
     public void initialize() {
         comboBox.setItems(FXCollections.observableArrayList("Claws (C)", "Teeth (T)", "Lasers (L)"));
         anchorGridPane.setMinSize(Region.USE_PREF_SIZE, Region.USE_PREF_SIZE);
+        createGrid(3, 3);
+    }
 
-        grid = new GridPane();
+    private void createGrid(int numRows, int numCols) {
+        grid.getChildren().clear();
+        anchorGridPane.getChildren().clear();
 
-        // Decide on a specific number of rows and columns for your grid.
-        int numRows = 5; // Example number of rows
-        int numCols = 5; // Example number of columns
+        // Create a brand-new grid with the valid row and column values
 
-        buttons = new Button[numRows][numCols];
+        buttons = new Button[numRows + 2][numCols + 2]; // Reset array of buttons
+        battle = new Battle(numRows, numCols); // Reset battle object
+        errorStatus.setText(null); // Remove any pre-existing error messages
+        grid = new GridPane(); // new GridPane
 
-        battle = new Battle(numRows, numCols);
-
-        for (int gridRow = 0; gridRow < numRows; gridRow++) {
-            for (int gridCol = 0; gridCol < numCols; gridCol++) {
-                Button button = new Button();
-                button.setPrefSize(30,30);
-                if (gridRow == 0 || gridCol == 0 || gridRow == numRows-1 || gridCol == numCols-1) {
-                    button.setText("#");
-                    button.setStyle("-fx-background-color: #D3D3D3;");
+        for (int gridRow = 0; gridRow < numRows+2; gridRow++) {
+            for (int gridCol = 0; gridCol < numCols+2; gridCol++) {
+                String buttonText = null; // Initialize button text
+                if (gridRow == 0 || gridCol == 0 || gridRow == numRows+1 || gridCol == numCols+1) {
+                    buttonText = "#"; // Establishes a wall on top of the button
                 }
-                grid.add(button, gridCol, gridRow); // Swap the parameters to the correct order
+
+                Button button = new Button(buttonText); // Creates button object with either null text or "#"
+                if ("#".equals(buttonText)) { button.setStyle("-fx-background-color: #D3D3D3;"); }
+                button.setPrefSize(30,30); // Preferred size for buttons
+                grid.add(button, gridCol, gridRow); // Add newly created button to space
 
                 buttons[gridRow][gridCol] = button;
                 int gridRowFR = gridRow;
@@ -100,20 +108,68 @@ public class MainController{
 
         anchorGridPane.getChildren().add(grid);
 
-        // Optionally, set the GridPane to anchor at all four sides to fill the AnchorPane.
         AnchorPane.setTopAnchor(grid, 10.0);
         AnchorPane.setLeftAnchor(grid, 10.0);
     }
 
-
     @FXML
     public void loadNewBattle() {
+        FileChooser fileChooser = new FileChooser();
 
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("TEXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(filter);
+
+        File file = fileChooser.showOpenDialog(grid.getScene().getWindow());
+
+        if (file != null) {
+            try {
+                battle = Reader.loadBattle(file);
+                if (battle != null) {
+                    updateGridPostLoad();
+                    fileStatus.setText("Battle loaded successfully!");
+                } else {
+                    fileStatus.setText("Battle could not be loaded.");
+                }
+            } catch (Exception e) {
+                fileStatus.setText("Battle could not be loaded.");
+            }
+        }
+    }
+
+    private void updateGridPostLoad() {
+        createGrid(battle.getRows(), battle.getColumns());
+        for (int battleRow = 0; battleRow < battle.getRows(); battleRow++) {
+            for (int battleCol = 0; battleCol < battle.getColumns(); battleCol++) {
+                Entity entity = battle.getEntity(battleRow, battleCol);
+                if (entity != null) {
+                    if (entity instanceof Wall) {
+                        buttons[battleRow+1][battleCol+1].setText("#");
+                        buttons[battleRow+1][battleCol+1].setStyle("-fx-background-color: #D3D3D3;");
+                    }
+                    else if (entity instanceof PredaCon || entity instanceof Maximal) {
+                        buttons[battleRow+1][battleCol+1].setText(String.valueOf(entity.getSymbol()));
+                    }
+                }
+            }
+        }
+    }
+
+    @FXML
+    public void saveAsNewBattle() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Battle As...");
+
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text Files", "*.txt"));
+        File battleFile = fileChooser.showSaveDialog(grid.getScene().getWindow());
+
+        if (battleFile != null) {
+
+        }
     }
 
     @FXML
     public void saveNewBattle() {
-        final FileChooser fileChooser = new FileChooser();
+        FileChooser fileChooser = new FileChooser();
         fileChooser.setInitialDirectory(new File("."));
         fileChooser.setInitialFileName("saved-battle.txt");
         File fileSave = fileChooser.showSaveDialog(new Stage());
@@ -171,48 +227,7 @@ public class MainController{
         }
 
         // Clear whatever was drawn before to make room for new grid
-        grid.getChildren().clear();
-        anchorGridPane.getChildren().clear();
-
-        // Create a brand-new grid with the valid row and column values
-
-        buttons = new Button[newRowCount + 2][newColCount + 2]; // Reset array of buttons
-        battle = new Battle(newRowCount, newColCount); // Reset battle object
-        errorStatus.setText(null); // Remove any pre-existing error messages
-
-        grid = new GridPane(); // new GridPane
-
-        for (int gridRow = 0; gridRow < newRowCount+2; gridRow++) {
-            for (int gridCol = 0; gridCol < newColCount+2; gridCol++) {
-                String buttonText = null; // Initialize button text
-                if (gridRow == 0 || gridCol == 0 || gridRow == newRowCount+1 || gridCol == newColCount+1) {
-                    buttonText = "#"; // Establishes a wall on top of the button
-                }
-
-                Button button = new Button(buttonText); // Creates button object with either null text or "#"
-                if ("#".equals(buttonText)) { button.setStyle("-fx-background-color: #D3D3D3;"); }
-                button.setPrefSize(30,30); // Preferred size for buttons
-                grid.add(button, gridCol, gridRow); // Add newly created button to space
-
-                buttons[gridRow][gridCol] = button;
-                int gridRowFR = gridRow;
-                int gridColFR = gridCol;
-
-                button.setOnAction(event -> handleButtonClick(gridRowFR, gridColFR));
-
-                button.setOnMouseExited(event -> displayButtonInfo.setText(null));
-
-                button.setOnMouseEntered(event -> handleMouseEntry(gridRowFR, gridColFR));
-            }
-        }
-
-        grid.setHgap(5);
-        grid.setVgap(5);
-
-        anchorGridPane.getChildren().add(grid);
-
-        AnchorPane.setTopAnchor(grid, 10.0);
-        AnchorPane.setLeftAnchor(grid, 10.0);
+        createGrid(newRowCount, newColCount);
     }
 
     private void handleMouseEntry(int gridRowFR, int gridColFR) {
