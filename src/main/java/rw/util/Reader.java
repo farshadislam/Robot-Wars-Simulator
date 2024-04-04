ab386d39788a4acb9982cc63749ff39b0760ce28
farshad.islam <farshad.islam@ucalgary.ca>
2024-04-04 13:13:50 -0600

    Javadoc and inline commenting

--- src/main/java/rw/util/Reader.java
+++ src/main/java/rw/util/Reader.java
@@ -1,3 +1,9 @@
+/**
+ * Author: Farshad Islam
+ * Tutorial: 07
+ * Tuesday, April 2nd, 2024
+ **/
+
 package rw.util;
 
 import rw.battle.Battle;
@@ -18,98 +24,104 @@ import java.util.HashSet;
  */
 public final class Reader {
 
+    /**
+     * Takes in a .txt file and creates a Battle object out of it
+     *
+     * @param file
+     * @return battle
+     */
     public static Battle loadBattle(File file) {
 
-        ArrayList<String> bLines = new ArrayList<>();
+        ArrayList<String> bLines = new ArrayList<>(); // Initialize amalgamation of all lines
 
-        Battle battle = null;
+        Battle battle = null; // Initialize new battle object
 
-        int rowCount = 0;
+        int rowCount = 0; // Arbitrary assignments
 
         int colCount = 0;
 
-        HashSet<String> robotSymbols = new HashSet<>();
+        HashSet<String> robotSymbols = new HashSet<>(); // Initialize tracker for all robotSymbols
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String battleLine;
-            while ((battleLine = br.readLine()) != null) {
-                bLines.add(battleLine);
+            BufferedReader br = new BufferedReader(new FileReader(file)); // Initialize BufferedReader
+            String battleLine; // String representation of lines
+            while ((battleLine = br.readLine()) != null) { // While-loop iterating over every line in the file
+                bLines.add(battleLine); // Strings get added to bLines
             }
             br.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e); // error handling
         }
 
 
-        for (int i = 0; i < 2; i++) {
-            if (i == 0) {
+        for (int i = 0; i < 2; i++) { // Only meant to iterate over the first two lines
+            if (i == 0) { // Map row length
                 try {
-                    rowCount = Integer.parseInt(bLines.get(i));
+                    rowCount = Integer.parseInt(bLines.get(i)); // String to integer
                 } catch (NumberFormatException e) {
-                    System.err.println("Invalid value for map dimensions!");
+                    System.err.println("Invalid value for map dimensions!"); // Error handling
                 }
             }
 
-            if (i == 1) {
+            if (i == 1) { // Map column length
                 try {
-                    colCount = Integer.parseInt(bLines.get(i));
+                    colCount = Integer.parseInt(bLines.get(i)); // String to integer
                 } catch (NumberFormatException e) {
-                    System.err.println("Invalid value for map dimensions!");
+                    System.err.println("Invalid value for map dimensions!"); // Error handling
                 }
             }
 
-            if (rowCount > 0 && colCount > 0) {battle = new Battle(rowCount, colCount);}
+            if (rowCount > 0 && colCount > 0) { battle = new Battle(rowCount, colCount); } // Only creates a battle if map sizes contribute to 2D array
         }
 
-        for (int i = 2; i < bLines.size(); i++) {
+        for (int i = 2; i < bLines.size(); i++) { // Remainder of lines in file all handled similarly
             assert battle != null;
-            String[] entity = bLines.get(i).split(",");
+            String[] entity = bLines.get(i).split(","); // Every line is turned into an array of index info
 
-            if (entity.length > 2) {
-                switch (entity[2]) {
-                    case "WALL":
-                        int wrow = Integer.parseInt(entity[0]);
+            if (entity.length > 2) { // Not just a coordinate
+                switch (entity[2]) { // Analyzes the third index position in the array
+                    case "WALL": // Identifies a wall
+                        int wrow = Integer.parseInt(entity[0]); // Get coordinate info
                         int wcol = Integer.parseInt(entity[1]);
 
-                        battle.addEntity(wrow, wcol, Wall.getWall());
+                        battle.addEntity(wrow, wcol, Wall.getWall()); // Place wall @ coordinate (wrow, wcol)
                         break;
 
-                    case "MAXIMAL":
-                        if (entity.length < 8) {
+                    case "MAXIMAL": // Identifies Maximal
+                        if (entity.length < 8) { // Needs a total of 8 arguments
                             throw new IllegalArgumentException("Invalid data format: Missing Maximal attributes.");
                         }
 
-                        if (entity[3].length() != 1) {
+                        if (entity[3].length() != 1) { // Needs a char variable for symbol
                             throw new IllegalArgumentException("Invalid Maximal symbol length: " + entity[3]);
                         }
 
-                        if (robotSymbols.contains(entity[3])) {
+                        if (robotSymbols.contains(entity[3])) { // Needs an original symbol
                             throw new IllegalArgumentException("Non-unique robot symbol: " + entity[3]);
                         }
 
-                        int mrow = Integer.parseInt(entity[0]);
+                        int mrow = Integer.parseInt(entity[0]); // Get coordinate info
                         int mcol = Integer.parseInt(entity[1]);
 
-                        if (mrow < 0 || mcol < 0 || mrow >= rowCount || mcol >= colCount) {
+                        if (mrow < 0 || mcol < 0 || mrow >= rowCount || mcol >= colCount) { // Must occur inside of array
                             throw new ArrayIndexOutOfBoundsException("Given Maximal coordinates outside of battle arena.");
                         }
 
-                        char mcREP = entity[3].charAt(0);
-                        robotSymbols.add(entity[3]);
+                        char mcREP = entity[3].charAt(0); // Creating all Maximal attributes once error handling occurs and no exceptions are found
+                        robotSymbols.add(entity[3]); // Will not let future symbols match this one
 
                         String maxName = entity[4];
                         int mHP = Integer.parseInt(entity[5]);
                         int mAttack = Integer.parseInt(entity[6]);
                         int mDefense = Integer.parseInt(entity[7]);
 
-                        Maximal maximal = new Maximal(mcREP, maxName, mHP, mAttack, mDefense);
-                        battle.addEntity(mrow, mcol, maximal);
+                        Maximal maximal = new Maximal(mcREP, maxName, mHP, mAttack, mDefense); // Creates new Maximal instance variable
+                        battle.addEntity(mrow, mcol, maximal); // Adds it to the battle
                         break;
 
                     case "PREDACON":
-                        if (entity.length < 7) {
-                            throw new IllegalArgumentException("Invalid data format: Missing PredaCon attributes.");
+                        if (entity.length < 7) { //
+                            throw new IllegalArgumentException("Invalid data format: Missing PredaCon attributes."); // Needs 7 attributes
                         }
 
                         if (entity[3].length() != 1) {
--- src/main/java/rw/util/Writer.java
+++ src/main/java/rw/util/Writer.java
@@ -1,3 +1,9 @@
+/**
+ * Author: Farshad Islam
+ * Tutorial: 07
+ * Tuesday, April 2nd, 2024
+ **/
+
 package rw.util;
 
 import rw.battle.*;
@@ -9,41 +15,46 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Writer {
-   public static void saveBattle(Battle battle, File file) {
-       ArrayList<String> battleDetails = new ArrayList<>();
-       battleDetails.add(String.valueOf(battle.getRows()));
-       battleDetails.add(String.valueOf(battle.getColumns()));
+    /**
+     *
+     * @params battle, file
+     * @returns N/A
+     */
+    public static void saveBattle(Battle battle, File file) {
+       ArrayList<String> battleDetails = new ArrayList<>(); // Takes in all lines as readable strings
+       battleDetails.add(String.valueOf(battle.getRows())); // Gets map row size
+       battleDetails.add(String.valueOf(battle.getColumns())); // Gets map column size
 
        for (int i = 0; i < battle.getRows(); i++) {
            for (int j = 0; j < battle.getColumns(); j++) {
-               Entity entity = battle.getEntity(i,j);
-               String battleLine = i + "," + j;
+               Entity entity = battle.getEntity(i,j); // Identify the Entity object in the battle coordinate
+               String battleLine = i + "," + j; // Always write out the coordinate first
 
-               if (entity instanceof PredaCon) {
+               if (entity instanceof PredaCon) { // Retrieves all relevant info for a PredaCon object
                    battleLine = battleLine + ",PREDACON," + entity.getSymbol() + "," + ((PredaCon) entity).getName() + "," + ((PredaCon) entity).getHealth() + "," + ((PredaCon) entity).getWeaponType();
                }
 
-               if (entity instanceof Maximal) {
+               if (entity instanceof Maximal) { // Retrieves all relevant info for a Maximal object
                    battleLine = battleLine + ",MAXIMAL," + entity.getSymbol() + "," + ((Maximal) entity).getName() + "," + ((Maximal) entity).getHealth() + "," + ((Maximal) entity).weaponStrength() + "," + ((Maximal) entity).armorStrength();
                }
 
-               if (entity instanceof Wall) {
+               if (entity instanceof Wall) { // Wall has no special attributes; it is just a wall
                    battleLine = battleLine + ",WALL";
                }
 
-               battleDetails.add(battleLine);
+               battleDetails.add(battleLine); // Completed line gets added to battleDetails
            }
        }
 
        try {
-           BufferedWriter bw = new BufferedWriter(new FileWriter(file.getName()));
+           BufferedWriter bw = new BufferedWriter(new FileWriter(file.getName())); // Initialize Writer.java
            for (String coordinateInfo : battleDetails) {
-               bw.write(coordinateInfo);
-               bw.write("\n");
+               bw.write(coordinateInfo); // Write every string that is in battleDetails
+               bw.write("\n"); // New line so that everything doesn't output to the same line
            }
            bw.close();
        } catch (IOException ex) {
-           throw new RuntimeException(ex);
+           throw new RuntimeException(ex); // Take in exception if this process fails
        }
 
    }
